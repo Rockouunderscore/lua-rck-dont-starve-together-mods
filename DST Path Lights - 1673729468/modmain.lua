537546fd92319f00be1928225a6280e598f8e918
rck <brandongauthier01@gmail.com>
2022-03-30 19:26:08 -0400

    Path_light fix attempt #1

--- DST Path Lights - 1673729468/modmain.lua
+++ DST Path Lights - 1673729468/modmain.lua
@@ -1,6 +1,5 @@
-   
-Assets=
-{
+
+Assets={
     Asset("ATLAS", "images/inventoryimages/path_light.xml"),
 }
 
@@ -9,38 +8,46 @@ PrefabFiles =
     "path_light",
 }
 
-STRINGS = GLOBAL.STRINGS
-RECIPETABS = GLOBAL.RECIPETABS
-Recipe = GLOBAL.Recipe
-Ingredient = GLOBAL.Ingredient
-TECH = GLOBAL.TECH
+-- STRINGS = GLOBAL.STRINGS
+-- RECIPETABS = GLOBAL.RECIPETABS
+-- Recipe = GLOBAL.Recipe
+-- Ingredient = GLOBAL.Ingredient
+-- TECH = GLOBAL.TECH
 
 GLOBAL.STRINGS.NAMES.PATH_LIGHT = "Path Light"
-STRINGS.RECIPE_DESC.PATH_LIGHT = "It lights your path."
+GLOBAL.STRINGS.RECIPE_DESC.PATH_LIGHT = "It lights your path."
 GLOBAL.STRINGS.CHARACTERS.GENERIC.DESCRIBE.PATH_LIGHT = "It's a light"
-    
+
 -----------------------------
 
 local path_lightrecipe = GLOBAL.Recipe(
     "path_light",
-    { 
-        Ingredient("lantern", 1),
-        Ingredient("lightbulb", 2),
+    {
+        GLOBAL.Ingredient("lantern", 1),
+        GLOBAL.Ingredient("lightbulb", 2),
     },
-    GLOBAL.RECIPETABS.LIGHT, GLOBAL.TECH.SCIENCE_TWO, "path_light_placer", 0, nil, nil, nil, "images/inventoryimages/path_light.xml" )
+    GLOBAL.RECIPETABS.LIGHT,
+    GLOBAL.TECH.SCIENCE_TWO,
+    "path_light_placer",
+    0,
+    nil,
+    nil,
+    nil,
+    "images/inventoryimages/path_light.xml"
+)
 
 --------------------------------
 
-local lightRadius = GetModConfigData("light_radius")
-if lightRadius == "small"
+local light_radius = GetModConfigData("light_radius")
+if light_radius == "small"
     then GLOBAL.lightRadius = 1
-elseif lightRadius == "medium"
+elseif light_radius == "medium"
     then GLOBAL.lightRadius = 2
-elseif lightRadius == "large"
+elseif light_radius == "large"
     then GLOBAL.lightRadius = 3
-elseif lightRadius == "larger"
+elseif light_radius == "larger"
     then GLOBAL.lightRadius = 4
-elseif lightRadius == "huge"
+elseif light_radius == "huge"
     then GLOBAL.lightRadius = 5
 else
     GLOBAL.lightRadius = 2
