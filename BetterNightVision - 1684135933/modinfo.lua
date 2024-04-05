3d92362e6eaaa9139f7454e4bdcc52b501818099
Rockou_ <brandongauthier01@gmail.com>
2024-04-04 21:48:50 -0400

    better night vision v17

--- BetterNightVision - 1684135933/modinfo.lua
+++ BetterNightVision - 1684135933/modinfo.lua
@@ -1,4 +1,4 @@
-name = "BetterNightVision V16"
+name = "BetterNightVision V17"
 description = [[Removes Moggles Black/Red effect and add nightvision with the selected Hotkey	(Default = X)
 The nightvision DOESN'T protect you from charlie (Unless you play in a world without caves(and you are host))
 
@@ -6,7 +6,7 @@ Thanks to Fuzzy Waffle and Tony
 The snippets of code they've contributed to are shown in the mod's 'modmain.lua' file]]
 
 author = "Rockou_ With Help From the Community"
-version = "16"
+version = "17"
 api_version_dst = 10
 
 dst_compatible = true
@@ -42,10 +42,62 @@ configuration_options = {
     {
         label = "Night vision Toggle Key",
         hover = "The key that will toggle night vision",
-        name = "nvk",
+        name = "NIGHTVISION_TOGGLE_KEY",
         options = keylist,
         default = "KEY_X",
-    }--,
+    },
+    {
+        label = "Enable Color Cubes Override",
+        hover = "Whether to override color cubes or not",
+        name = "NIGHTVISION_OVERRIDE_COLOR_CUBES",
+        options = {
+            {
+                description = "Enabled",
+                data = true
+            },
+            {
+                description = "Disabled",
+                data = false
+            }
+        },
+        default = true
+    },
+    {
+        label = "Color Cubes Override Value",
+        hover = "Color Cubes to use",
+        name = "NIGHTVISION_USE_COLORCUBES",
+        options = {
+            { description = "Default",         data = "",                                                  hover = "Keep what would be the current filter, without moggle vision"},
+            { description = "Identity",        data = "images/colour_cubes/identity_colourcube.tex",       hover = "Default \"Identity\" filter"},
+            { description = "Autumn Day",      data = "images/colour_cubes/day05_cc.tex",                  hover = "Autumn Day Filter"},
+            { description = "Winter Day",      data = "images/colour_cubes/snow_cc.tex",                   hover = "Winter Day Filter"},
+            { description = "Winter Night",    data = "images/colour_cubes/night04_cc.tex",                hover = "Winter Night Filter"},
+            { description = "Spring Day",      data = "images/colour_cubes/spring_day_cc.tex",             hover = "Spring Day Filter"},
+            { description = "Spring Dusk",     data = "images/colour_cubes/spring_dusk_cc.tex",            hover = "Spring Dusk Filter"},
+            { description = "Summer Day",      data = "images/colour_cubes/summer_day_cc.tex",             hover = "Summer Day Filter"},
+            { description = "Summer Dusk",     data = "images/colour_cubes/summer_dusk_cc.tex",            hover = "Summer Dusk Filter"},
+            { description = "Summer Night",    data = "images/colour_cubes/summer_night_cc.tex",           hover = "Summer Night Filter"},
+            { description = "Caves",           data = "images/colour_cubes/caves_default.tex",             hover = "Caves Filter"},
+            { description = "Ruins Calm",      data = "images/colour_cubes/ruins_dark_cc.tex",             hover = "DST: Ruins Calm Phase Filter"},
+            { description = "Ruins Warning",   data = "images/colour_cubes/ruins_dim_cc.tex",              hover = "DST: Ruins Warning Phase Filter"},
+            { description = "Ruins Wild",      data = "images/colour_cubes/ruins_light_cc.tex",            hover = "DST: Ruins Wild Phase Filter"},
+            { description = "Were Form",       data = "images/colour_cubes/beaver_vision_cc.tex",          hover = "Woodie's Were Form Filter"},
+            { description = "Fungus",          data = "images/colour_cubes/fungus_cc.tex",                 hover = "DST: Fungus Forest Filter"},
+            { description = "Moon Storm",      data = "images/colour_cubes/moonstorm_cc.tex",              hover = "Moon Storm Filter"},
+            { description = "Full Moon",       data = "images/colour_cubes/purple_moon_cc.tex",            hover = "Full Moon Filter"},
+            { description = "Lunacy",          data = "images/colour_cubes/lunacy_regular_cc.tex",         hover = "DST: Lunacy Filter"},
+            { description = "Ghost",           data = "images/colour_cubes/ghost_cc.tex",                  hover = "DST: Ghost Filter"}
+            --[[
+            {
+                description = "CC Name",
+                data = "CC Directory Path",
+                hover = "CC Config Tip - Not Required"
+            },
+            ]]
+        },
+        default = true
+    }
+    --,
     --{
     --    label = "CC table override Toggle Key",
     --    hover = "The key that will toggle color cube table override",
--- BetterNightVision - 1684135933/modmain.lua
+++ BetterNightVision - 1684135933/modmain.lua
@@ -4,21 +4,22 @@ local function GetKeyFromConfig(config)
     return key and (type(key) == "number" and key or GLOBAL[key])
 end
 
--- day05_cc is fall day
--- dusk03_cc is fall night
--- night03_cc is fall night
--- purple_moon_cc is fullmoon
--- ruins_light_cc is danger time in ruins
+local NIGHTVISION_TOGGLE_KEY = GetKeyFromConfig("NIGHTVISION_TOGGLE_KEY")
+---@type boolean
+local NIGHTVISION_OVERRIDE_COLOR_CUBES = GetModConfigData("NIGHTVISION_OVERRIDE_COLOR_CUBES")
 
-local NEW_NIGHTVISION_COLOURCUBES =
-{
-    day = "images/colour_cubes/day05_cc.tex",
-    dusk = "images/colour_cubes/day05_cc.tex",
-    night = "images/colour_cubes/day05_cc.tex",
-    full_moon = "images/colour_cubes/day05_cc.tex",
+---@type string
+local NIGHTVISION_USE_COLORCUBES = GetModConfigData("NIGHTVISION_USE_COLORCUBES")
+
+---@type table<string, string>
+local OVERRIDE_COLOR_CUBES = {
+    day = NIGHTVISION_USE_COLORCUBES,
+    dusk = NIGHTVISION_USE_COLORCUBES,
+    night = NIGHTVISION_USE_COLORCUBES,
+    full_moon = NIGHTVISION_USE_COLORCUBES
 }
 
---Thanks to Tony - Can be found in the mod's comments
+-- Thanks to Tony - Can be found in the mod's comments
 local function InGame()
     return GLOBAL.ThePlayer and GLOBAL.ThePlayer.HUD and not GLOBAL.ThePlayer.HUD:HasInputFocus()
 end
@@ -26,25 +27,37 @@ end
 
 local function ToggleNightVision()
 
-    local playervision = GLOBAL.ThePlayer.components.playervision
+    local nightvision_active = not GLOBAL.ThePlayer.components.playervision:HasNightVision()
 
-    local newNightVisionActive = not playervision:HasNightVision()
+    -- apply night vision
 
-    playervision:ForceNightVision(newNightVisionActive)
+    GLOBAL.ThePlayer.components.playervision:ForceNightVision(nightvision_active)
 
-    -- if
-    -- active   => NIGHTVISION_COLOURCUBES
-    -- else     => nil
-    playervision:SetCustomCCTable(newNightVisionActive and NEW_NIGHTVISION_COLOURCUBES or nil)
+    -- GLOBAL.ThePlayer:PushEvent("nightvision", nightvision_active)
 
-    GLOBAL.ThePlayer.components.talker:Say("Night Vision: "..tostring(newNightVisionActive))
+    -- apply color cubes
+
+    if NIGHTVISION_OVERRIDE_COLOR_CUBES then
+
+        local active_colour_cubes = nightvision_active and NIGHTVISION_USE_COLORCUBES ~= "" and OVERRIDE_COLOR_CUBES or nil
+
+        -- GLOBAL.ThePlayer.components.playervision:SetCustomCCTable(active_colour_cubes)
+
+        GLOBAL.ThePlayer:PushEvent("ccoverrides", active_colour_cubes)
+        GLOBAL.ThePlayer:PushEvent("ccphasefn", nil)
+    end
+
+    -- notify through talker
+
+    GLOBAL.ThePlayer.components.talker:Say("Night Vision: "..tostring(nightvision_active))
 
 end
 
-GLOBAL.TheInput:AddKeyUpHandler(GetKeyFromConfig("nvk"), function ()
+
+GLOBAL.TheInput:AddKeyUpHandler(NIGHTVISION_TOGGLE_KEY, function ()
     if not InGame() then return end --Thanks to Tony - Can be found in the mod's comments
-        if GLOBAL.ThePlayer == nil then return else --Thanks to Fuzzy Waffle - Can be found in the mod's Discusions "Bugs / Crashs"
-            ToggleNightVision()
-        end
+    if GLOBAL.ThePlayer == nil then return else --Thanks to Fuzzy Waffle - Can be found in the mod's Discusions "Bugs / Crashs"
+        ToggleNightVision()
+    end
 end
 )
